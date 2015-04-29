3cc7826f1ed36f17e74efa509690d93d87882549
Matthew Andrews <matt@mattandre.ws>
2015-04-29 15:39:47 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function(opts) {
+	var username = opts.username || process.env.SAUCE_USER;
+	var accessKey = opts.accessKey || SAUCE_KEY;
+	var passed = opts.passed || false;
+	return fetch('https://saucelabs.com/rest/v1/' + username + '/jobs/' + opts.sessionId, {
+		method: 'PUT',
+		headers: {
+			'Authorization': 'Basic ' + new Buffer(username + ':' + accessKey).toString('base64'),
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			passed: passed
+		})
+	});
+};
