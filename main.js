49d8e28da90ddbbbb9d08fd80d28d7ba8c3fa2d5
Matthew Andrews <matt@mattandre.ws>
2015-04-29 15:40:44 +0100

    Fix silly mistake

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 module.exports = function(opts) {
 	var username = opts.username || process.env.SAUCE_USER;
-	var accessKey = opts.accessKey || SAUCE_KEY;
+	var accessKey = opts.accessKey || process.env.SAUCE_KEY;
 	var passed = opts.passed || false;
 	return fetch('https://saucelabs.com/rest/v1/' + username + '/jobs/' + opts.sessionId, {
 		method: 'PUT',
